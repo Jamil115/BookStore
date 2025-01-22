be70a1e4d649b02a0906bb754b9bc0ea5012a978
Jamil115 <jamil71001@gmail.com>
2025-01-23 01:31:28 +0600

    add accesstoken, refreshtoken and cookie

--- backend/src/controllers/loginUser.controller.js
+++ backend/src/controllers/loginUser.controller.js
@@ -1,7 +1,29 @@
 import { User } from "../models/user.model.js";
 import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import bcrypt from "bcrypt"
+
+
+const generateAccessAndRefreshToken = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+
+        const accessToken =  user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        console.log("user.refreshToken: ", user.refreshToken)
+        // user.save({validateBeforeSave: false})
+
+        console.log("User: ",user)
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new apiError(500, "Something went wrong while generating access and refresh token")
+    }
+}
+
 
 const loginUser = asyncHandler(async (req,res)=> {
     
@@ -21,15 +43,36 @@ const loginUser = asyncHandler(async (req,res)=> {
     if(!user)
         throw new apiError(404, "User does not exists");
 
-    // const isPasswordValid = user.isPasswordCorrect(password);
     const isPasswordValid = await bcrypt.compare(password, user.password)
 
     if(!isPasswordValid)
         throw new apiError(401, "Invalid credentials")
 
+    console.log("User_ID: ",user._id)
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id);
+
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    // console.log("accessToken: ", accessToken)
+    // console.log("refreshToken: ", refreshToken)
+
     return res
     .status(200)
-    .json("User logged in successfully")
+    .cookie("accessToken", accessToken,options)
+    .cookie("refreshToken", refreshToken,options)
+    .json(
+        new apiResponse(
+            201,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            }
+        )
+    )
 })
 
 export {loginUser}
