358e666bf0190ce80bd6b3874256cedf7e4dd2cf
Jamil115 <jamil71001@gmail.com>
2025-02-02 02:44:20 +0600

    import cors and cookie parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,17 @@
 import express from "express"
-const app = express()
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
+const app = express()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || '*',
+    Credentials: true
+}))
+
+// app.use(cors)
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded( {extended: true, limit:"16kb"} ))
+app.use(cookieParser())
 
 import commonRouter from "./routes/common.routes.js"
 
