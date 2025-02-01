057275b93cc9b86a7d5dc3111e6ed914cdcb3562
Jamil115 <jamil71001@gmail.com>
2025-02-02 03:04:53 +0600

    added simple footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='bg-zinc-400 text-center h-14 pt-4'>
+            <p>&copy; 2025, Made by Jamil Khan</p>
+        </div>
+    )
+}
