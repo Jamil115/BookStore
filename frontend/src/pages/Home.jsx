91e0597286b9ccabc46963f2cae964dece609b16
Jamil115 <jamil71001@gmail.com>
2025-02-02 03:01:01 +0600

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Hero from '../components/Home/Hero'
+
+export default function Home() {
+  return (
+    <div>
+        <Hero />
+    </div>
+  )
+}
